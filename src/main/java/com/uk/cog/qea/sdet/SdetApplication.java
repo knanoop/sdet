3ee7e09051bd0c58a9a84ed4dd4c0b5615a2a01c
thiru <thiru@LAPTOP-A0E8JN6G.Home>
2018-05-14 15:00:40 +0100

    deploy changes

--- src/main/java/com/uk/cog/qea/sdet/SdetApplication.java
+++ src/main/java/com/uk/cog/qea/sdet/SdetApplication.java
@@ -2,6 +2,8 @@ package com.uk.cog.qea.sdet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,8 +13,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com.uk.cog.qea.sdet.assembler.dto.entity")
 @ComponentScan("com.uk.cog.qea.sdet.service")
 @EnableJpaRepositories("com.uk.cog.qea.sdet.repository")
-public class SdetApplication {
+public class SdetApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SdetApplication.class);
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(SdetApplication.class, args);
 	}
